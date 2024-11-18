263758e6c2dee4928c7cb1072aa07874fe61c553
keita nakano <keitatanakano@gmail.com>
2024-11-18 20:28:29 +0900

    追加

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO events (name, event_date, registrations, status, satisfaction_rate, revenue)
-VALUES 
-('テクノロジーカンファレンス2023', '2023-09-15', 350, '開催中', 4.5, 1750000),
-('ウェブ開発ワークショップ', '2023-10-01', 150, '準備中', 0, 750000),
-('ビジネスにおけるAIセミナー', '2023-10-15', 80, '登録受付中', 0, 400000); 
