1bf9ff0b12896637cdcb9e8be27a3d4aa89d4ff2
Tobi (Jitera) <developers@jitera.com>
2024-11-18 12:18:11 +0000

    fix: 1 issues from deployment report

--- src/main/java/com/example/livedemo/LivedemoApplication.java
+++ src/main/java/com/example/livedemo/LivedemoApplication.java
@@ -2,12 +2,13 @@ package com.example.livedemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example.livedemo"})
 public class LivedemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LivedemoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(LivedemoApplication.class, args);
+    }
 }
